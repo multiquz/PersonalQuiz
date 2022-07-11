c9658d78dbe5fedce16221d0cb90dd7e3a4426ca
Vladimir <vproshchin@gmail.com>
2022-07-11 19:59:05 +0300

    Get rid of optional unwrap in findResultingAnimal method

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -47,7 +47,7 @@ class ResultViewController: UIViewController {
         
         let animalsSorted = animalsNumber.sorted { $0.1 > $1.1 }
         
-        guard let resultingAnimal = animalsSorted.first?.key as? Animal else { return nil }
+        let resultingAnimal = animalsSorted.first?.key
         
         return resultingAnimal
     }
